0b34a53042da49fef3be91be82032c209da251f5
Levid <jassontejada@gmail.com>
2023-08-23 13:02:43 -0400

    Adding interface API

--- app/src/main/java/com/levid/rickandmortyapp/data/source/remote/RickAndMortyAPI.kt
+++ app/src/main/java/com/levid/rickandmortyapp/data/source/remote/RickAndMortyAPI.kt
@@ -1,4 +1,19 @@
-package com.levid.rickandmortyapp.data.resources.remote
+package com.levid.rickandmortyapp.data.source.remote
+
+import com.levid.rickandmortyapp.data.source.remote.dto.CharacterDto
+import com.levid.rickandmortyapp.data.source.remote.dto.CharactersDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RickAndMortyAPI {
+    @GET("character/")
+    suspend fun getCharacters(
+        @Query("page") page : Int
+    ): CharactersDto
+
+    @GET("character/{id}")
+    suspend fun getCharacter(
+        @Path("id") id : Int
+    ): CharacterDto
 }
